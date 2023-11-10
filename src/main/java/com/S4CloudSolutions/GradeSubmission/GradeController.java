acac4f397a168dbdeb9fba844d966a6604ceddfd
Scott <ScottWilliamsSolutions@gmail.com>
2023-11-09 22:03:14 -0500

    Controller and form HTML/CSS

---
+++ src/main/java/com/S4CloudSolutions/GradeSubmission/GradeController.java
@@ -0,0 +1,13 @@
+package com.S4CloudSolutions.GradeSubmission;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GradeController {
+
+    @GetMapping("/grades")
+    public String getGrades() {
+        return "grades";
+    }
+}
