2e810706f2d0da2f4b13f91395a896abc4ded5a0
Scott <ScottWilliamsSolutions@gmail.com>
2023-11-11 10:12:16 -0500

    refactor for Array list for each grade entry

--- src/main/java/com/S4CloudSolutions/GradeSubmission/GradeController.java
+++ src/main/java/com/S4CloudSolutions/GradeSubmission/GradeController.java
@@ -1,16 +1,23 @@
 package com.S4CloudSolutions.GradeSubmission;
 
-import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class GradeController {
 
+    List<Grade> studentGrades = Arrays.asList(
+        new Grade("Harry", "Potions", "B-"),
+        new Grade("Hermoine", "Potions", "A+"),
+        new Grade("Ron", "Potions", "C-")
+    );
     @GetMapping("/grades")
     public String getGrades(Model model) {
-        Grade grade = new Grade("Harry", "Potions", "C-");
-        model.addAttribute("grade", grade);
+        model.addAttribute("grades", studentGrades);
         return "grades";
     }
 }
