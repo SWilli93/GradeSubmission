5e99b3d53f8dcef7de93cdd81e54bf7d30b66538
Scott <ScottWilliamsSolutions@gmail.com>
2023-11-11 10:32:54 -0500

    deletion of FormController.java, addition of form submission logic/functionality, renaming of Mark for grades table

--- src/main/java/com/S4CloudSolutions/GradeSubmission/GradeController.java
+++ src/main/java/com/S4CloudSolutions/GradeSubmission/GradeController.java
@@ -3,21 +3,31 @@ package com.S4CloudSolutions.GradeSubmission;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class GradeController {
 
-    List<Grade> studentGrades = Arrays.asList(
-        new Grade("Harry", "Potions", "B-"),
-        new Grade("Hermoine", "Potions", "A+"),
-        new Grade("Ron", "Potions", "C-")
-    );
+    List<Grade> studentGrades = new ArrayList<>();
+
     @GetMapping("/grades")
     public String getGrades(Model model) {
         model.addAttribute("grades", studentGrades);
         return "grades";
+    }
+
+    @GetMapping("/")
+    public String gradeForm(Model model) {
+        model.addAttribute("grade", new Grade());
+        return "form";
+    }
+
+    @PostMapping("/handleSubmit")
+    public String submitGrade(Grade grade) {
+        studentGrades.add(grade);
+        return "redirect:/grades";
     }
 }
