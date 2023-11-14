0727ec93ef118b5673018d86e52455cc5c050fa8
Scott <ScottWilliamsSolutions@gmail.com>
2023-11-13 19:37:24 -0500

    refactor and fix error where updating created two copies of entry

--- src/main/java/com/S4CloudSolutions/GradeSubmission/GradeController.java
+++ src/main/java/com/S4CloudSolutions/GradeSubmission/GradeController.java
@@ -14,21 +14,33 @@ public class GradeController {
 
     List<Grade> studentGrades = new ArrayList<>();
 
-    @GetMapping("/grades")
-    public String getGrades(Model model, @RequestParam(required = false) String name) {
-        model.addAttribute("grades", studentGrades);
-        return "grades";
-    }
-
     @GetMapping("/")
-    public String gradeForm(Model model) {
-        model.addAttribute("grade", new Grade());
+    public String gradeForm(Model model, @RequestParam(required = false) String name) {
+        model.addAttribute("grade", getGradeIndex(name) == -1000 ? new Grade() : studentGrades.get(getGradeIndex(name)));
         return "form";
     }
 
     @PostMapping("/handleSubmit")
     public String submitGrade(Grade grade) {
-        studentGrades.add(grade);
+        int index = getGradeIndex(grade.getName());
+        if (index == -1000) {
+            studentGrades.add(grade);
+        } else {
+            studentGrades.set(index, grade);
+        }
         return "redirect:/grades";
+    }
+
+    @GetMapping("/grades")
+    public String getGrades(Model model) {
+        model.addAttribute("grades", studentGrades);
+        return "grades";
+    }
+
+    public Integer getGradeIndex(String name) {
+        for (int i = 0; i < studentGrades.size(); i++) {
+            if (studentGrades.get(i).getName().equals(name)) return i;
+        }
+        return -1000;
     }
 }
